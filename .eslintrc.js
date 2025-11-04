5ec7fa4361cbd675ca9a4470c654f3014dded7a3
amanullah7649 <mdamanullah7649@gmail.com>
2025-11-05 02:33:12 +0600

    added github action

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'no-var': 'error',
-    'no-unused-vars': 'error',
     'prefer-const': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off'
