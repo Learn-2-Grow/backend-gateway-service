c580c1f46f7f01f3cb2af71ecdc84007049faebb
amanullah7649 <mdamanullah7649@gmail.com>
2025-11-05 01:59:05 +0600

    show lint errro

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   async health() {
+    var newVal;
+
     const health = {
       message: 'Server is running....!!!',
     };
