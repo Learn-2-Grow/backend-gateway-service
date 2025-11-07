05c10171ef1ac638101921986e2a6626e532bd5c
amanullah7649 <mdamanullah7649@gmail.com>
2025-11-08 02:16:08 +0600

    added cicd  process document

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { envValidator } from './common/validators/env.validator';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: envValidator,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
